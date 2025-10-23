9f9b12d99a76c6547b445ee59190fc6c5b4593aa
domson <qeeu.que@gmail.com>
2025-10-23 18:39:16 +0200

    RootNavigator

---
+++ quizioo/src/navigation/RootNavigator.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/Login/LoginScreen";
+
+export type RootStackParamList = {
+  Login: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
