728944fda31f7a1159891c7bef15bec53f7b9bea
domson <qeeu.que@gmail.com>
2025-10-24 12:55:27 +0200

    HomeScreen

---
+++ quizioo/src/screens/Home/HomeScreen.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { colors, styles } from "./HomeScreen.styles";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {RootStackParamList} from "../../navigation/RootNavigator";
+import {useNavigation} from "@react-navigation/native";
+
+type Nav = NativeStackNavigationProp<RootStackParamList, 'Home'>;
+
+export default function HomeScreen() {
+	const navigation = useNavigation<Nav>();
+
+	const handleSignIn = () => Alert.alert("New Quiz clicked");
+
+  return (
+    <SafeAreaView style={styles.container}>
+		<View style={styles.frame}>
+			<View style={styles.content}>
+				<Text style={styles.title}>Home</Text>
+
+				<View style={styles.buttons}>
+					<TouchableOpacity style={styles.button} onPress={handleSignIn}>
+						<Text style={styles.buttonText}>New Quiz</Text>
+					</TouchableOpacity>
+
+				</View>
+			</View>
+		</View>
+    </SafeAreaView>
+  );
+}
+
