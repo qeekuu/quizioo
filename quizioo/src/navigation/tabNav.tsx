e21249fb4f67d16c28b52556cac18903af842e2d
domson <qeeu.que@gmail.com>
2025-10-26 17:56:29 +0100

    tabNavBar

--- quizioo/src/navigation/tabNav.tsx
+++ quizioo/src/navigation/tabNav.tsx
@@ -13,14 +13,38 @@ const Tab = createBottomTabNavigator<TabParamList>();
 
 export default function tabNav(){
 	return (
-		<Tab.Navigator screenOptions={{ headerShown: false }}>
+		<Tab.Navigator screenOptions={{ 
+			tabBarActiveTintColor: "#17B9C4", 
+			tabBarInactiveTintColor: "#fff",  
+			headerShown: false,
+			tabBarStyle:{
+				backgroundColor: "#0E1422",
+				borderTopWidth: 1,
+				borderBlockColor: "#141B2C",
+				height: 100,
+				paddingBottom: 30,
+				paddingTop: 10,
+			},
+			tabBarLabelStyle: {
+				fontSize: 10,
+				fontWeight: "bold",
+			}
+		}}>
 			<Tab.Screen
 				name="Home"
 				component={HomeScreen}
+				options={{
+					title: "Home",
+					tabBarIcon: ({ color, size }) => <Ionicons name ="home" color={color} size={size} />
+				}}
 			/>
 			<Tab.Screen
 				name="QuizAdd"
 				component={QuizAddScreen}
+				options={{
+					title: "QuizAdd",
+					tabBarIcon: ({ color, size }) => <Ionicons name ="add" color={color} size={size} />
+				}}
 			/>
 		</Tab.Navigator>
 	);
