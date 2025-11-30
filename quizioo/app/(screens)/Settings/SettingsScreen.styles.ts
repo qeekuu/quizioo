a32f8c0c5ecdac3463c32808a1f1ded2e78a5c6b
domson <qeeu.que@gmail.com>
2025-11-30 14:51:54 +0100

    Settings screen layout (1)

--- quizioo/app/(screens)/Settings/SettingsScreen.styles.ts
+++ quizioo/app/(screens)/Settings/SettingsScreen.styles.ts
@@ -10,6 +10,7 @@ export const colors = {
   textPlaceholder: "#6B7280",
   stroke: "#2B3248",
   inputBg: "#111826",
+  bordercolor: "#fd86b4",
 };
 
 
@@ -19,25 +20,66 @@ export const styles = StyleSheet.create({
     backgroundColor: colors.background,
   },
 
-  content: {
-	width: "100%",
-    maxWidth: 420,      
+  text: {
+	color: colors.text,
   },
 
-  /*TOP BAR */
   topBar: {
-	flexDirection: "row",
-	alignItems: "center",
-	justifyContent: "space-between",
-	marginBottom: 20,
+	backgroundColor: colors.background,
+	height: 70,
+	borderRadius: 10,
+	justifyContent: 'center',
+	alignItems: 'center',
+	flexDirection: 'row',
+	color: colors.background,
+	paddingLeft: 15,
+	paddingRight: 8,
+	margin: 10,
+	marginBottom: 25,
+	borderBottomWidth: 7,
+	borderBottomColor: colors.bordercolor,
   },
-	
-  settingLabel: {
-	fontSize: 22,
-	letterSpacing: 1,
+
+  topBarText: {
 	color: colors.text,
-	fontWeight: "700",
-	marginTop: 8,
+	letterSpacing: 0.7,
+	fontWeight: 'bold',
+	fontSize: 17,
+  },
+
+  settingsSectionText: {
+	color: colors.text,
+	letterSpacing: 0.7,
+	fontWeight: 'bold',
+	fontSize: 17,
+  },
+
+  settingsSubSectionText: {
+	color: colors.textMuted,
+	letterSpacing: 0.7,
+	fontWeight: 'bold',
+	fontSize: 13,
+  },
+
+  settingsSection: {
+	justifyContent: 'space-between',
+	alignItems: 'center',
+	flexDirection: 'row',
+	height: 70,
+	margin: 10,
+	paddingLeft: 25,
+	paddingRight: 25,
+	borderRadius: 25,
+	borderWidth: 5,
+	borderColor: colors.bordercolor,
+  },
+
+  settingsSectionHeader: {
+	color: colors.text,
+	letterSpacing: 0.7,
+	fontWeight: 'bold',
+	fontSize: 17,
+	margin: 10,
   },
 
 });
--- quizioo/app/(screens)/Settings/SettingsScreen.tsx
+++ quizioo/app/(screens)/Settings/SettingsScreen.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { Alert, Text, TextInput, TouchableOpacity, View, StatusBar, FlatList } from "react-native";
+import React, { lazy, use, useState } from "react";
+import { Alert, Text, TextInput, TouchableOpacity, View, StatusBar, FlatList, SectionList, Switch } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { colors, styles } from "./SettingsScreen.styles";
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
-import { RootStackParamList } from "@/app/(navigation)/RootNavigator";
+import { RootStackParamList } from "@/app/(navigation)/types";
 import {useNavigation} from "@react-navigation/native";
 
 type Nav = NativeStackNavigationProp<RootStackParamList, 'Settings'>;
@@ -11,31 +11,104 @@ type Nav = NativeStackNavigationProp<RootStackParamList, 'Settings'>;
 export default function HomeScreen() {
 	const navigation = useNavigation<Nav>();
 
-	const handleSignOut = (name: string) => {
+	const [isDarkMode, setisDarkMode] = useState(true);
+	const [isNot, setNot] = useState(false);
+	const [isCamNot, setCamOn] = useState(false);
+
+	const handleSignOut = () => {
 		navigation.reset({
 			index: 0,
 			routes: [{name: "Login"}],
 		})
 	}
 
-  return (
-	<SafeAreaProvider>
-		<SafeAreaView style={styles.container}>
-			<StatusBar barStyle={"light-content"} />
-				<View style={styles.content}>
-					<View style={styles.topBar}>
-						<View>
-							<Text style={styles.settingLabel}>Settings</Text>
-							<TouchableOpacity
-								onPress={() => handleSignOut("Sign out")}
-							>
-								<Text>Tekst</Text>
-							</TouchableOpacity>
-						</View>	
-					</View>	
-				</View>
+	const SETTINGS = [
+		{
+			title: "General Settings",
+			data: [
+				{ key: "Display", label: "Light or Dark"},
+				{ key: "Notifications", label: "Yes or No" }
+			],
+		},
+		{
+			title: "Privacy and Security",
+			data: [
+				{ key: "Permissions", label: "Camera"},
+			],
+		},
+		{
+			title: "Account",
+			data: [
+				{ key: "Profile", label: "Chec Your Profile"},
+				{ key: "Sign Out", label: "See you soon!"}
+			],
+		},
+		{
+			title: "Support",
+			data: [
+				{ key: "Contact", label: "Label"},
+			],
+		},
 
+	];
+
+  return (
+	  <SafeAreaView style={styles.container}>
+		<View style={styles.topBar}>
+			<Text style={styles.topBarText}>Settings</Text>
+		</View>
+
+		<SectionList
+        sections={SETTINGS}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <View style={styles.settingsSection}>
+			<View>
+				<Text style={styles.settingsSectionText}>{item.key}</Text>
+			
+				<Text style={styles.settingsSubSectionText}>{item.label}</Text>
+			</View>
+				{item.key == "Display" && (
+					<Switch
+						value={isDarkMode}
+						onValueChange={(value) => setisDarkMode(value)}
+					/>
+				)}
+
+				{item.key == "Notifications" && (
+					<Switch
+						value={isNot}
+						onValueChange={(value) => setNot(value)}
+					/>
+				)}
+				{item.key == "Permissions" && (
+					<Switch
+						value={isCamNot}
+						onValueChange={(value) => setCamOn(value)}
+					/>
+				)}
+				{item.key == "Profile" && (
+					<Switch
+						value={isCamNot}
+						onValueChange={(value) => setCamOn(value)}
+					/>
+				)}
+				{item.key == "Sign Out" && (
+					<TouchableOpacity onPress={handleSignOut}>
+						<Text style={styles.settingsSubSectionText}>Sign Out</Text>	
+					</TouchableOpacity>
+				)}
+	
+	
+	
+		  </View>
+        )}
+        renderSectionHeader={({ section }) => (
+          <View>
+            <Text style={styles.settingsSectionHeader}>{section.title}</Text>
+          </View>
+        )}
+      />
 		</SafeAreaView>
-	</SafeAreaProvider>
   );
 }
