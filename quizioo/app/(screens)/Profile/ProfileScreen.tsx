467e20fea4139ec4b613dd2f184b5387040483db
domson <qeeu.que@gmail.com>
2025-11-30 14:52:17 +0100

    Profile & Quizzes

---
+++ quizioo/app/(screens)/Profile/ProfileScreen.tsx
@@ -0,0 +1,14 @@
+import { Rect } from "react-native-safe-area-context";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function ProfileScreen()
+{
+	return (
+		<SafeAreaView>
+			<View>
+				<Text>Progile</Text>
+			</View>
+		</SafeAreaView>
+	);
+}
---
+++ quizioo/app/(screens)/Quizzes/QuizzesScreen.tsx
@@ -0,0 +1,15 @@
+import { Rect } from "react-native-safe-area-context";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Quizzes()
+{
+	return (
+		<SafeAreaView>
+			<View>
+				<Text>QUizzes</Text>
+			</View>
+		</SafeAreaView>
+	);
+}
+
