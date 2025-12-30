957b3d97711234cec9b691fd8c90e804654dbc34
domson <qeeu.que@gmail.com>
2025-12-30 23:12:04 +0100

    static profile screen v1

---
+++ quizioo/app/(screens)/Profile/ProfileScreen.styles.ts
@@ -0,0 +1,123 @@
+import { StyleSheet } from "react-native";
+
+export const colors = {
+  background: "#0E1422",
+  surface: "#141B2C",
+  link: "#17B9C4",
+  onPrimary: "#FFFFFF",
+  text: "#F8FAFC",
+  textMuted: "#A3ADC2",
+  textPlaceholder: "#6B7280",
+  stroke: "#2B3248",
+  inputBg: "#111826",
+  bordercolor: "#fd86b4"
+};
+
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: colors.background,
+	},
+
+	text: {
+		color: colors.text,
+	},
+
+	textMuted: {
+		color: colors.textMuted,
+	},
+
+    textLink: {
+		color: colors.link,
+    },
+
+	topBar: {
+		height: 70,
+		borderRadius: 10,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flexDirection: 'row',
+		color: colors.background,
+		paddingLeft: 15,
+		paddingRight: 15,
+		margin: 10,
+		marginBottom: 25,
+		borderBottomWidth: 7,
+		borderBottomColor: colors.bordercolor,
+	},
+
+	topBarText: {
+		color: colors.text,
+		letterSpacing: 0.7,
+		fontWeight: 'bold',
+		fontSize: 17,
+	},
+
+	profilePicture: {
+		height: 150,
+		width: 150,
+		justifyContent: 'center',
+		alignItems: 'center',
+		alignSelf: 'center',
+		paddingLeft: 15,
+		paddingRight: 15,
+		margin: 10,
+		borderRadius: 75,
+		borderWidth: 7,
+		borderColor: colors.bordercolor,
+	},
+
+	staticticsCard: {
+		paddingLeft: 15,
+		paddingRight: 15,
+		margin: 10,
+		borderTopWidth: 7,
+		borderTopColor: colors.bordercolor,
+	},
+
+	button: {
+		height: 75,
+		width: '100%',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		padding: 12,
+		marginTop: 16,
+		borderRadius: 25,
+		borderWidth: 5,
+		borderColor: colors.bordercolor,
+	},
+
+	buttonText: {
+		color: colors.text,
+		fontWeight: 'bold',
+		fontSize: 17,
+	},
+
+	buttonIcon: {
+		width: 40,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginRight: 15,
+	},
+
+	achievements:{
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		margin: 10,
+	},
+
+	achievementsText: {
+		color: colors.text,
+		letterSpacing: 0.7,
+		fontWeight: 'bold',
+		fontSize: 17,
+    },
+
+	achievementsCard: {
+
+	}
+
+});
--- quizioo/app/(screens)/Profile/ProfileScreen.tsx
+++ quizioo/app/(screens)/Profile/ProfileScreen.tsx
@@ -1,13 +1,35 @@
 import { Rect } from "react-native-safe-area-context";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { styles, colors } from "../Profile/ProfileScreen.styles";
+import {Ionicons} from "@expo/vector-icons";
+import { Link } from "@react-navigation/native";
 
 export default function ProfileScreen()
 {
 	return (
-		<SafeAreaView>
-			<View>
-				<Text>Progile</Text>
+		<SafeAreaView style={styles.container}>
+			<View style={styles.topBar}>
+				<Text style={styles.topBarText}>Profile</Text>
+			</View>
+			<View style={styles.profilePicture}>
+				<Text style={styles.text}>profpic</Text>
+			</View>
+			<View style={styles.staticticsCard}>
+				<TouchableOpacity style={styles.button}>
+					<View style={styles.buttonIcon}>
+						<Ionicons name="settings" size={32} color="rgba(100, 200, 255, 0.8)" />
+					</View>
+					<Text style={styles.buttonText}>Your settings</Text>	
+				</TouchableOpacity>
+			</View>
+            {/* Zmienić Link */}
+			<View style={styles.achievements}>
+				<Text style={styles.achievementsText}>Achievements</Text>
+				<Link screen="QuizAdd" params={{ id: 'quizadd' }}style={styles.textLink}>See all</Link>
+			</View>
+			<View style={styles.achievementsCard}>
+
 			</View>
 		</SafeAreaView>
 	);
