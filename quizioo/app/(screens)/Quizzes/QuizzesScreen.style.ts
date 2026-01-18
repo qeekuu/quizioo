9187731bf8079361cf59bfe3871a2046ebd300d2
Majkel2003 <michal.now.2003@wp.pl>
2026-01-18 20:51:16 +0100

    Paginacji

--- quizioo/app/(screens)/Quizzes/QuizzesScreen.style.ts
+++ quizioo/app/(screens)/Quizzes/QuizzesScreen.style.ts
@@ -67,8 +67,18 @@ export const styles = StyleSheet.create({
         margin: 10,
     },
 
+    searchInput: {
+        width: "100%",
+        padding: 10,
+        marginTop: 12,
+        marginBottom: 12,
+        borderRadius: 25,
+        color: colors.text,
+        backgroundColor: colors.surface,
+    },
+
     button: {
-        height: 52,
+        height: 32,
         marginBottom: 12,
         justifyContent: 'center',
         alignItems: 'center',
@@ -83,13 +93,121 @@ export const styles = StyleSheet.create({
         fontWeight: "700"
     },
 
-    searchInput: {
-        paddingLeft: 25,
-        paddingRight: 25,
+    dropdown: {
+        width: "100%",
+        height: 45,
+        padding: 10,
+        borderRadius: 25,
+        backgroundColor: colors.surface,
+    },
+
+    dropdownPlaceholder: {
+        color: colors.textMuted,
+    },
+
+    dropdownSelectedText: {
+        color: colors.text,
+        fontSize: 14,
+    },
+
+    dropdownListContainer: {
+        backgroundColor: colors.surface,
+        borderRadius: 16,
+        paddingVertical: 6,
+        borderWidth: 1,
+        borderColor: colors.bordercolor,
+    },
+
+    dropdownItemContainer: {
+        borderRadius: 12,
+        marginHorizontal: 6,
+    },
+
+    dropdownItemText: {
+        color: colors.text,
+        fontSize: 14,
+    },
+
+    dropdownActiveColor: {
+
+    },
+
+    containerItem: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        height: 75,
         margin: 10,
         borderRadius: 25,
+        backgroundColor: colors.background,
         borderWidth: 5,
-        borderColor: colors.bordercolor
-    }
+        borderColor: colors.bordercolor,
+        paddingHorizontal: 20,
+    },
 
-});
+    leftIcon: {
+        width: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 15,
+    },
+
+    centerText: {
+        flex: 1,
+    },
+
+    rightArrow: {
+        width: 30,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+    },
+
+    quizArrow: {
+        color: colors.textMuted,
+        fontSize: 17,
+        fontWeight: 'bold',
+    },
+
+    title: {
+        color:colors.text,
+    },
+
+    subTitle: {
+        color: colors.textMuted,
+    },
+
+    paginationRow: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginTop: 'auto',
+        paddingTop: 8,
+        paddingBottom: 8,
+    },
+
+    pagBtn: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: "#2a2f36",
+    },
+
+    pagBtnDisabled: {
+        opacity: 0.4,
+    },
+
+    pagBtnText: {
+        color: colors.text,
+        fontWeight: "600",
+    },
+
+    pagBtnTextDisabled: {
+        color: "#666b75",
+    },
+
+    pageInfo: {
+        color: colors.text,
+        fontSize: 14,
+        opacity: 0.8,
+    },
+}
--- quizioo/app/(screens)/Quizzes/QuizzesScreen.tsx
+++ quizioo/app/(screens)/Quizzes/QuizzesScreen.tsx
@@ -1,11 +1,138 @@
-import { Rect } from "react-native-safe-area-context";
-import { Text, View, TouchableOpacity, TextInput } from "react-native";
+import { Text, View, TouchableOpacity, TextInput, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "../Quizzes/QuizzesScreen.style";
 import { Dropdown } from "react-native-element-dropdown";
+import { useEffect, useState, useRef } from "react";
+import { Category, Quiz } from "@/app/(api)/types";
+import { api } from "@/app/(api)/client";
+import { Ionicons } from "@expo/vector-icons";
+import {FlatList} from "react-native";
 
 export default function QuizzesScreen()
 {
+    const QUIZZES_PER_PAGE = 3;
+
+    const [quizCategory, setQuizCategory] = useState<string | null>(null);
+    const [quizzes, setQuizzes] = useState<Quiz[]>([]);
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [quizLoading, setQuizLoading] = useState(true);
+    const [catLoading, setCatLoading] = useState(true);
+    const [search, setSearch] = useState("");
+    const [loading, setLoading] = useState<boolean>(false);
+    const [page, setPage] = useState<number>(1);
+    const [hasMore, setHasMore] = useState<boolean>(true);
+
+    const abortRef = useRef<AbortController | null>(null);
+    const requestIdRef = useRef(0);
+
+    useEffect(() => {
+        let mounted = true;
+        (async () => {
+            try {
+                setCatLoading(true);
+
+                const cats = await api.listCategories();
+                if(!mounted)
+                    return;
+                setCategories(cats);
+            } catch (e: any) {
+                if(!mounted)
+                    return;
+                Alert.alert("Error", e?.message ?? "Cannot load data");
+            } finally {
+                if(mounted) {
+                    setCatLoading(false);
+                }
+            }
+        })();
+
+        load(1);
+
+        return () => {
+            mounted = false;
+            abortRef.current?.abort();
+        };
+    }, []);
+
+    useEffect(() => {
+        // gdy user zmienia search/kategorię, wracamy na stronę 1
+        load(1);
+    }, [quizCategory, search]);
+
+    type ItemProps = {quiz: Quiz};
+
+    const Item = ({ quiz }: ItemProps) => (
+        <TouchableOpacity style={styles.containerItem}>
+            <View style={styles.leftIcon}>
+                <Ionicons name="book-sharp" size={32} color="rgba(100, 200, 255, 0.8" />
+            </View>
+
+            <View style={styles.centerText}>
+                <Text style={styles.title}>{quiz.quizName}</Text>
+                <Text style={styles.subTitle}>{quiz.quizType}</Text>
+            </View>
+
+            <View style={styles.rightArrow}>
+                <Text style={styles.quizArrow}>{'>'}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+
+    {/*const filteredQuizzes = quizzes.filter((q) => {
+		const matchesCategory = !quizCategory || q.quizType === quizCategory;
+
+		const matchesSearch = search.trim().length === 0 || q.quizName.toLowerCase().includes(search.trim().toLowerCase());
+
+		return matchesCategory && matchesSearch;
+	});
+	*/}
+
+    async function load(pageToLoad = 1) {
+        abortRef.current?.abort();
+        const controller = new AbortController();
+        abortRef.current = controller;
+
+        const myRequestid = ++requestIdRef.current;
+
+        setLoading(true);
+        try {
+            const { data, total, next } = await api.listQuizzes({
+                page: pageToLoad,
+                perPage: QUIZZES_PER_PAGE,
+                q: search,
+                quizType: quizCategory,
+                signal: controller.signal,
+            });
+
+            if(myRequestid !== requestIdRef.current)
+                return; // jeśli nowy pojawił się w międzyczasie to inoruj ten wynik
+            // console.log("Return form API:", { data, total });
+            console.log("PAGE DEBUG:", { pageToLoad, got: data.length, total, next, });
+
+            setQuizzes(data);
+            setPage(pageToLoad);
+            setHasMore(next !== null);
+
+        } catch (e) {
+            if((e as any)?.name === "AbortError")
+                return;
+            Alert.alert("Error", (e as Error).message);
+        } finally {
+            if(myRequestid === requestIdRef.current)
+                setLoading(false);
+        }
+    };
+
+    async function onNextPage() {
+        if (!hasMore || loading) return;
+        await load(page + 1);
+    };
+
+    async function onPrevPage() {
+        if (page <= 1 || loading) return;
+        await load(page - 1);
+    };
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.topBar}>
@@ -13,23 +140,77 @@ export default function QuizzesScreen()
             </View>
             <View style={styles.searchCard}>
                 <Text style={styles.searchCardText}>Filters:</Text>
-                <TextInput
-                    style={styles.searchInput}
-                    placeholder="Select category..."
-                    placeholderTextColor={"#FFFFFF"}
-                >
+                <Dropdown
+                    data={categories}
+                    labelField="label"
+                    valueField="label"
+                    placeholder={catLoading ? "Loading..." : "Select category..."}
+                    disable={catLoading || categories.length === 0}
+                    value={quizCategory}
+                    onChange={(item: Category) => setQuizCategory(item.label)}
 
-                </TextInput>
+                    style={styles.dropdown}
+                    placeholderStyle={styles.dropdownPlaceholder}
+                    selectedTextStyle={styles.dropdownSelectedText}
+                    containerStyle={styles.dropdownListContainer}
+                    itemTextStyle={styles.dropdownItemText}
+                    itemContainerStyle={styles.dropdownItemContainer}
+                    activeColor={styles.dropdownActiveColor}
+                />
 
                 <TextInput
                     style={styles.searchInput}
                     placeholder="Search for quizzes..."
                     placeholderTextColor={"#FFFFFF"}
+                    value={search}
+                    onChangeText={setSearch}
+                />
+                <TouchableOpacity
+                    onPress={() => {
+                        setQuizCategory(null);
+                        setSearch("");
+                    }}
+                    disabled={!quizCategory && search.trim().length === 0}
+                    style={styles.button}
                 >
-
-                </TextInput>
+                    <Text style={styles.text}>Reset filters</Text>
+                </TouchableOpacity>
             </View>
 
+            <View style={{ flex: 1 }}>
+                <FlatList
+                    data={quizzes}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({ item }) => <Item quiz={item} />}
+                    ListEmptyComponent={
+                        !loading ? (
+                            <Text style={{ textAlign: "center", color: "#aaa", marginTop: 20 }}>
+                                No quizzes available
+                            </Text>
+                        ) : null
+                    }
+                />
+                <View style={styles.paginationRow}>
+                    <TouchableOpacity
+                        style={[styles.pagBtn, (page <= 1 || loading) && styles.pagBtnDisabled,]}
+                        onPress={onPrevPage}
+                        disabled={page <= 1 || loading}
+                    >
+                        <Text style={[styles.pagBtnText, (page <= 1 || loading) && styles.pagBtnTextDisabled,]}>Prev</Text>
+                    </TouchableOpacity>
+
+                    <Text style={styles.pageInfo}>Strona {page}</Text>
+
+                    <TouchableOpacity
+                        style={[styles.pagBtn, (!hasMore || loading) && styles.pagBtnDisabled,]}
+                        onPress={onNextPage}
+                        disabled={!hasMore || loading}
+                    >
+                        <Text style={[styles.pagBtnText, (!hasMore || loading) && styles.pagBtnTextDisabled,]}>Next</Text>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
         </SafeAreaView>
     );
 }
