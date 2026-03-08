050f99bd8f5eb5f9c62cc2d2bd4235b787e0efcf
qeekuu <qeeu.que@gmail.com>
2026-03-08 21:50:46 +0100

    talwind colors + components name

--- quizioo/app/(quizzes)/[quizId].tsx
+++ quizioo/app/(quizzes)/[quizId].tsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const quizDetails = () => {
+const QuizDetails = () => {
 	return (
 		<View>
-			<Text>quizDetails</Text>
+			<Text>QuizDetails</Text>
 		</View>
 	)
 }
 
-export default quizDetails
+export default QuizDetails
--- quizioo/tailwind.config.js
+++ quizioo/tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
 	presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+			colors: {
+				primary: "#FD86D4",
+				secondary: "#F5F5F5",
+				accent: "#FF5722",
+			},
+		},
   },
   plugins: [],
 }
