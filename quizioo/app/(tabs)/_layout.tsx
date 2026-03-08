096ec0c92f7ea5f961d3e640b8c846da4770a6d9
qeekuu <qeeu.que@gmail.com>
2026-03-08 21:51:32 +0100

    tabBarIcons using TabIcon component

--- quizioo/app/(tabs)/_layout.tsx
+++ quizioo/app/(tabs)/_layout.tsx
@@ -1,15 +1,19 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import TabIcon from "@/components/TabIcon";
 
 const _Layout = () => {
 	return (
-		<Tabs>
+		<Tabs
+	
+		>
 			<Tabs.Screen
 				name="home"
 				options={{
 					title: 'Home',
 					headerShown: false,
+					tabBarIcon: ({ size, focused }) => <TabIcon name="home" color={focused ? "#FD86D4" : "#000"} size={32} />
 				}}
 			/>
 			<Tabs.Screen
@@ -17,6 +21,7 @@ const _Layout = () => {
 				options={{
 					title: 'Profile',
 					headerShown: false,
+					tabBarIcon: ({ size, focused }) => <TabIcon name="person" color={focused ? "#FD86D4" : "#000"} size={32} />
 				}}
 			/>
 			<Tabs.Screen
@@ -24,6 +29,7 @@ const _Layout = () => {
 				options={{
 					title: 'Settings',
 					headerShown: false,
+					tabBarIcon: ({ size, focused }) => <TabIcon name="settings" color={focused ? "#FD86D4" : "#000"} size={32} />
 				}}
 			/>
 		</Tabs>
