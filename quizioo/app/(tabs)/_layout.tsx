315358df8d62a6a1296d410e7d9947ff20713298
qeekuu <qeeu.que@gmail.com>
2026-03-10 10:29:17 +0100

    basic tab navigation

--- quizioo/app/(tabs)/_layout.tsx
+++ quizioo/app/(tabs)/_layout.tsx
@@ -6,14 +6,29 @@ import TabIcon from "@/components/TabIcon";
 const _Layout = () => {
 	return (
 		<Tabs
-	
+			screenOptions={{
+				tabBarStyle: {
+					backgroundColor: "#0E1422",
+					borderTopWidth: 1,
+					borderTopColor: "#141B2C",
+					height: 100,
+					paddingBottom: 8,
+					paddingTop: 8,
+					elevation: 0,
+					shadowOpacity: 0,
+				},
+				tabBarLabelStyle: {
+					fontSize: 12,
+					fontWeight: "600",
+				}
+			}}
 		>
 			<Tabs.Screen
 				name="home"
 				options={{
 					title: 'Home',
 					headerShown: false,
-					tabBarIcon: ({ size, focused }) => <TabIcon name="home" color={focused ? "#FD86D4" : "#000"} size={32} />
+					tabBarIcon: ({ size, focused }) => <TabIcon name={focused ? "home" : "home-outline"} color={focused ? "#FD86D4" : "#fff"} size={focused ? 28 : 22} />
 				}}
 			/>
 			<Tabs.Screen
@@ -21,7 +36,7 @@ const _Layout = () => {
 				options={{
 					title: 'Profile',
 					headerShown: false,
-					tabBarIcon: ({ size, focused }) => <TabIcon name="person" color={focused ? "#FD86D4" : "#000"} size={32} />
+					tabBarIcon: ({ size, focused }) => <TabIcon name={focused ? "person" : "person-outline"} color={focused ? "#FD86D4" : "#fff"} size={focused ? 28: 22} />
 				}}
 			/>
 			<Tabs.Screen
@@ -29,7 +44,7 @@ const _Layout = () => {
 				options={{
 					title: 'Settings',
 					headerShown: false,
-					tabBarIcon: ({ size, focused }) => <TabIcon name="settings" color={focused ? "#FD86D4" : "#000"} size={32} />
+					tabBarIcon: ({ size, focused }) => <TabIcon name={focused ? "settings" : "settings-outline"} color={focused ? "#FD86D4" : "#fff"} size={focused ? 28 : 22} />
 				}}
 			/>
 		</Tabs>
